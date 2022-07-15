80974bdd4faf980db8ad2e07f573233e721ca999
dekapd99 <dekaprimatio@gmail.com>
2022-07-15 20:21:22 +0700

    Initial Commit

---
+++ CrypTraces/CrypTracesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypTracesApp.swift
+//  CrypTraces
+//
+//  Created by Deka Primatio on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CrypTracesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
